76334f29f64f21757b8d97ecf55ca667e673bc98
J1 <kellyrenner15@gmail.com>
2026-02-12 16:59:09 +0800

    添加一点信息

--- app.py
+++ app.py
@@ -72,3 +72,13 @@ def terms():
 if __name__ == "__main__":
     debug = os.environ.get("FLASK_DEBUG", "0") == "1"
     app.run(host="0.0.0.0", port=5000, debug=debug)
+
+    from flask import Response
+
+
+    @app.route("/robots.txt")
+    def robots():
+        return Response(
+            "User-agent: *\nAllow: /\n\nSitemap: https://wuelefo.com/sitemap.xml",
+            mimetype="text/plain"
+        )
